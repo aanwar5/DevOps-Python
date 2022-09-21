41cc4aeb6bbdd24813f059b7286200479e7f1590
EC2 Default User <ec2-user@ip-172-31-17-10.ap-northeast-1.compute.internal>
2022-09-21 04:34:41 +0000

    Changing req.txt

--- app.py
+++ app.py
@@ -1,43 +1,7 @@
-#!/usr/bin/env python
-import click
+import tensorflow_hub as hub
 
-def change(amount):
-    # calculate the resultant change and store the result (res)
-    res = []
-    coins = [1, 5, 10, 25]  # value of pennies, nickels, dimes, quarters
-    coin_lookup = {25: "quarters", 10: "dimes", 5: "nickels", 1: "pennies"}
+model = hub.KerasLayer("https://tfhub.dev/google/nnlm-en-dim128/2")
+embeddings = model(["The rain in Spain.", "falls",
+                      "mainly", "In the plain!"])
 
-    # divide the amount*100 (the amount in cents) by a coin value
-    # record the number of coins that evenly divide and the remainder
-    coin = coins.pop()
-    num, rem = divmod(int(amount * 100), coin)
-    # append the coin type and number of coins that had no remainder
-    res.append({num: coin_lookup[coin]})
-
-    # while there is still some remainder, continue adding coins to the result
-    while rem > 0:
-        coin = coins.pop()
-        num, rem = divmod(rem, coin)
-        if num:
-            if coin in coin_lookup:
-                res.append({num: coin_lookup[coin]})
-    return res
-
-@click.command()
-@click.option(
-    "--amount",
-    prompt="Amount: ",
-    help="Creates change for dollar and cents value:  i.e. 1.34",
-)
-def make_change(amount):
-    """Gives Correct Change"""
-
-    result = change(float(amount))
-    click.echo(click.style(f"Change for {amount}:", fg="red"))
-    for correct_change in result:
-        for num, coin in correct_change.items():
-            click.echo(click.style(f"{coin}: {num}", fg="green"))
-
-if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    make_change()
+print(embeddings.shape)  #(4,128)
