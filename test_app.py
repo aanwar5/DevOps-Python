e36cf320c3bf7d3bfd8cd567b90536591568742e
EC2 Default User <ec2-user@ip-172-31-17-10.ap-northeast-1.compute.internal>
2022-09-22 02:06:31 +0000

    Changes to test_app.py

--- test_app.py
+++ test_app.py
@@ -1,13 +1,5 @@
-import app
-from click.testing import CliRunner
+from app import change
 
 
 def test_change():
-    assert [{5: "quarters"}, {1: "nickels"}, {4: "pennies"}] == app.change(1.34)
-
-
-def test_app():
-    runner = CliRunner()
-    result = runner.invoke(app.make_change, ["--amount", "1.34"])
-    assert result.exit_code == 0
-    assert "quarters: 5" in result.output
+    assert [{5: "quarters"}, {1: "nickels"}, {4: "pennies"}] == change(1.34)
